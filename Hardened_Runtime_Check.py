7692791e953dc650d83b483d547bf845d83044ac
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-12-09 10:31:48 -0500

    Add files via upload

--- Hardened_Runtime_Check.py
+++ Hardened_Runtime_Check.py
@@ -12,7 +12,7 @@ for each in d:
 			for item in p:
 				apath2 = apath + "/" + item
 				try:
-					x = subprocess.getstatusoutput('codesign --display --verbose %s' % apath2)
+					x = subprocess.getstatusoutput('codesign --display --verbose "%s"' % apath2)
 					if (('flags' in x[1]) and ('runtime' in x[1])):
 						print("\033[92m[+] Hardened runtime enabled in: %s\033[0m" % apath2)
 					elif (('flags' in x[1]) and ('runtime' not in x[1])):
@@ -30,7 +30,7 @@ for binary in c:
 	pth = "/usr/local/bin/%s" % binary
 	try:
 		if os.access(pth, os.R_OK):
-			z = subprocess.getstatusoutput('codesign --display --verbose %s' % pth)
+			z = subprocess.getstatusoutput('codesign --display --verbose "%s"' % pth)
 			if (('flags' in z[1]) and ('runtime' in z[1])):
 				print("\033[92m[+] Hardened runtime enabled in: %s\033[0m" % pth)
 			elif (('flags' in z[1]) and ('runtime' not in z[1])):
@@ -49,7 +49,7 @@ for bin in b:
 	pth2 = "/usr/sbin/%s" % bin
 	try:
 		if os.access(pth2, os.R_OK):
-			y = subprocess.getstatusoutput('codesign --display --verbose %s' % pth2)
+			y = subprocess.getstatusoutput('codesign --display --verbose "%s"' % pth2)
 			if (('flags' in y[1]) and ('runtime' in y[1])):
 				print("\033[92m[+] Hardened runtime enabled in: %s\033[0m" % pth2)
 			elif (('flags' in y[1]) and ('runtime' not in y[1])):
@@ -67,7 +67,7 @@ a = os.listdir("/usr/bin")
 for file in a:
 	pth3 = "/usr/bin/%s" % file
 	try:
-		q = subprocess.getstatusoutput('codesign --display --verbose %s' % pth3)
+		q = subprocess.getstatusoutput('codesign --display --verbose "%s"' % pth3)
 		if (('flags' in q[1]) and ('runtime' in q[1])):
 			print("\033[92m[+] Hardened runtime enabled in: %s\033[0m" % pth3)
 		elif (('flags' in q[1]) and ('runtime' not in q[1])):
